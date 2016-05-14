8f487eacf5039a210cbb772109a3f54f08633261
anhkien96 <anhkien96@gmail.com>
2016-05-15 01:13:05 +0700

    Updated

---
+++ README.md
@@ -0,0 +1,17 @@
+## FreeCodeCamp API: Request Header Parser Microservice
+By Kien Nguyen <anhkien96@gmail.com>
+
+### User stories
+
+I can get the IP address, language and operating system for my browser.
+
+### Example usage
+```
+http://fcc-api-request-header.herokuapp.com/whoami
+```
+### Example output
+```
+{"ipaddress":"127.0.0.1","language":"vi","software":"Windows NT 6.3; WOW64"}
+```
+### Live site
+[http://fcc-api-request-header.herokuapp.com](http://fcc-api-request-header.herokuapp.com)
---
+++ index.js
@@ -0,0 +1,24 @@
+require('http').createServer(function(req, res) {
+
+	if (req.url.slice(1, 7) == 'whoami') {
+		var ip = req.headers['x-forwarded-for'] ||
+			req.connection.remoteAddress ||
+			req.socket.remoteAddress ||
+			req.connection.socket.remoteAddress;
+
+		res.end(
+			JSON.stringify(
+				{
+					ipaddress: ip,
+					language: req.headers['accept-language'].split(',')[0],
+					software: req.headers['user-agent'].match(/\((.*?)\)/)[1]
+				}
+			)
+		);
+	}
+	else require('fs').fs.readFile('public/header-parse.html', function(err, data) {
+		if (err) throw err;
+		res.end(data);
+	});
+	
+}).listen(process.env.PORT);
